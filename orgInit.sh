eef3dbfa0e702f941c36361b5bda3d5417441af0
zhou059 <zhou059@gmail.com>
2024-08-07 08:50:56 -0700

    cleanup

--- orgInit.sh
+++ orgInit.sh
@@ -15,7 +15,7 @@ sfdx shane:user:password:set -g User -l User -p salesforce1
 # Create User
 #sfdx force:user:create FirstName="Quentin" LastName="Engineer" Alias="Quentin" profileName="System Administrator"
 #sfdx force:user:create FirstName="Jeff" LastName="Ronald" Alias="Jeff" profileName="System Administrator"
-sfdx force::user:create FirstName="Quentin" LastName="Engineer" Alias="Quentin" profileName="System Administrator" 
+sfdx force:user:create FirstName="Quentin" LastName="Engineer" Alias="Quentin" profileName="System Administrator" 
 sfdx force:user:create FirstName="Tim" LastName="Service" Alias="Tim" profileName="System Administrator" 
 sfdx force:user:create FirstName="Sue" LastName="Marketing" Alias="Sue" profileName="System Administrator" 
 sfdx force:user:create FirstName="Cindy" LastName="Sales" Alias="Cindy" profileName="System Administrator" 
